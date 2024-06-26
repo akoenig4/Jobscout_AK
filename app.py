95b298133db4700f8531c9fc145547aee4d7cb8d
ocaplan <ocaplan@mail.yu.edu>
2024-06-26 16:48:01 -0400

    Add app.py from querying-job-listings branch

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("JobScout")
+st.text(
+    "JobScout is a web application that simplifies job searching by querying multiple job \nlisting sites and saving "
+    "user-defined searches. Utilizing a distributed work \nscheduler, it regularly updates the job listings database "
+    "and notifies users of new \nopportunities. The application also features a user-friendly web interface for "
+    "\nmanual queries and real-time results.")
+
+
+### GET GOOGLE SSO INTEGRATED WITH STREAMLIT - https://discuss.streamlit.io/t/google-authentication-in-a-streamlit-app/43252 ###
