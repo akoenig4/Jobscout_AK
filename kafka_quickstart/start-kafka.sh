c8ee68647a959a39e5541eb3a37aa359de3f4f81
akoenig4 <akoenig4@mail.yu.edu>
2024-06-26 21:15:16 -0400

    dockerized kafka quickstart

--- kafka_quickstart/start-kafka.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create Kafka config file with advertised listeners
-echo "advertised.listeners=PLAINTEXT://$HOSTNAME:9092" > /opt/kafka/config/server.properties
-
-# Start Kafka server
-exec /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
--- kafka_quickstart/start-zookeeper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Start Zookeeper using its configuration file
-exec /opt/zookeeper/bin/zookeeper-server-start.sh /opt/zookeeper/conf/zookeeper.properties
