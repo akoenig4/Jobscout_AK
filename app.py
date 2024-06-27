f91aabd130dc4251f38af6f414c57ca96432e92c
ocaplan <ocaplan@mail.yu.edu>
2024-06-27 13:59:52 -0400

    added a drop-down list for location

--- app.py
+++ app.py
@@ -7,8 +7,18 @@ st.text(
     "and notifies users of new \nopportunities. The application also features a user-friendly web interface for "
     "\nmanual queries and real-time results.")
 
-job_title = st.text_input("Job Title:") # can add default value
-location = st.text_input("Location:") # can add default value
+job_title = st.text_input("Job Title:") # make empty string the default for entire U.S.
+states = [
+    '', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+    'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+    'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan',
+    'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+    'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
+    'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
+    'Wisconsin', 'Wyoming'
+]
+location = st.selectbox(label="Location:", options=states)
 company = st.text_input("Company:") # can add default value
 
 
