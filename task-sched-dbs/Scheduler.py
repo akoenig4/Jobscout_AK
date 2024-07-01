093a8bedc6a35c4c4afb8d95d00dc7528e7a716f
akoenig4 <akoenig4@mail.yu.edu>
2024-07-01 15:56:34 -0400

    now dynamoDB can actually create the tables

--- task-sched-dbs/Scheduler.py
+++ task-sched-dbs/Scheduler.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timezone
 import isodate
 from Tables import Tables, Task, Mode
-from boto3.exceptions import ClientError 
+from botocore.exceptions import ClientError
+
 
 
 
@@ -30,7 +31,7 @@ class Scheduler:
             )
 
             self.table_set.executions.put_item(
-                Execution_Item={
+                Item={
                     'task_id': t.task_id,
                     'next_exec_time': datetime.now() + isodate.parse_duration(t.interval),
                     'segment': self.get_next_segment()
@@ -40,8 +41,7 @@ class Scheduler:
         except ClientError as e:
             error_code = e.response['Error']['Code']
             error_message = e.response['Error']['Message']
-            print(f"Error putting item in DynamoDB: {
-                  error_code} - {error_message}")
+            print(f"Error putting item in DynamoDB: {error_code} - {error_message}")
             # Handle specific error cases here
 
         except Exception as e:
@@ -51,6 +51,25 @@ class Scheduler:
     def get_next_segment(self):
         self.segment += 1
         return self.segment
+         
+    def query_executions_by_next_exec_time(self, next_exec_time):
+        try:
+            response = self.table_set.executions.query(
+                KeyConditionExpression=Key('next_exec_time').eq(next_exec_time)
+            )
+
+            for item in response['Items']:
+                print(item)  # Process each item as needed -- eventually return them so they can be executed
+
+        except ClientError as e:
+            error_code = e.response['Error']['Code']
+            error_message = e.response['Error']['Message']
+            print(f"Error querying executions table: {error_code} - {error_message}")
+            # Handle specific error cases here
+
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            # Handle unexpected errors or log them for investigation
 
     def get_unix_timestamp_by_min(self, dt: datetime) -> int:
        
@@ -61,3 +80,7 @@ class Scheduler:
         # Convert to UNIX timestamp
         unix_timestamp = int(dt.timestamp())
         return unix_timestamp
+
+if __name__ == "__main__":
+    print("main is running")
+    scheduler = Scheduler()
