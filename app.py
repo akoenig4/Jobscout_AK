d29fc11ec393d724994ad6ec5ee4680b60d7dfbf
ocaplan <ocaplan@mail.yu.edu>
2024-07-02 11:28:30 -0400

    commented out DynamoDB stuff

--- app.py
+++ app.py
@@ -24,41 +24,41 @@ states = [
 location = st.selectbox(label="Location:", options=states) # make empty string the default for entire U.S.
 company = st.text_input("Company:") # can add default value
 
-# st.button("search") # job title and location - also make sure they are filled out
-
-# Initialize a DynamoDB resource
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-
-# Replace 'JobListings' with your DynamoDB table name
-table_name = 'JobListings'
-
-
-def read_jobs(job_title=None, location=None, company=None):
-    table = dynamodb.Table(table_name)
-    scan_kwargs = {}
-    filter_expression = None
-    if job_title:
-        filter_expression = Attr('job_title').eq(job_title)
-    if location:
-        filter_expression = filter_expression & Attr('location').eq(location) if filter_expression else Attr(
-            'location').eq(location)
-    if company:
-        filter_expression = filter_expression & Attr('company').eq(company) if filter_expression else Attr(
-            'company').eq(company)
-
-    if filter_expression:
-        scan_kwargs['FilterExpression'] = filter_expression
-
-    response = table.scan(**scan_kwargs)
-    return response.get('Items', [])
-
-
-if st.button("Search"):
-    if job_title or location or company:
-        results = read_jobs(job_title, location, company)
-        if results:
-            st.write("Results:", results)
-        else:
-            st.write("No matching jobs found.")
-    else:
-        st.error("Please enter at least one search criteria.")
+st.button("search") # job title and location - also make sure they are filled out
+#
+# # Initialize a DynamoDB resource
+# dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+#
+# # Replace 'JobListings' with your DynamoDB table name
+# table_name = 'JobListings'
+#
+#
+# def read_jobs(job_title=None, location=None, company=None):
+#     table = dynamodb.Table(table_name)
+#     scan_kwargs = {}
+#     filter_expression = None
+#     if job_title:
+#         filter_expression = Attr('job_title').eq(job_title)
+#     if location:
+#         filter_expression = filter_expression & Attr('location').eq(location) if filter_expression else Attr(
+#             'location').eq(location)
+#     if company:
+#         filter_expression = filter_expression & Attr('company').eq(company) if filter_expression else Attr(
+#             'company').eq(company)
+#
+#     if filter_expression:
+#         scan_kwargs['FilterExpression'] = filter_expression
+#
+#     response = table.scan(**scan_kwargs)
+#     return response.get('Items', [])
+#
+#
+# if st.button("Search"):
+#     if job_title or location or company:
+#         results = read_jobs(job_title, location, company)
+#         if results:
+#             st.write("Results:", results)
+#         else:
+#             st.write("No matching jobs found.")
+#     else:
+#         st.error("Please enter at least one search criteria.")
