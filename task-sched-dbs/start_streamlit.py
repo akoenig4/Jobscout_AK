7b271ec7d8927ba6aa48646d239dae4f09bcd9ff
akoenig4 <akoenig4@mail.yu.edu>
2024-07-19 16:49:37 -0400

    updated fastapi URL to use elastic IP

--- task-sched-dbs/start_streamlit.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def run_streamlit():
-    subprocess.run(["streamlit", "run", "app_experiment.py"])  # Ensure "streamlit_app.py" is the name of your Streamlit script
-
-if __name__ == "__main__":
-    run_streamlit()
