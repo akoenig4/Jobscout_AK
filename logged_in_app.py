79a983e406c90adb553edc9a7a8d266585262034
akoenig4 <akoenig4@mail.yu.edu>
2024-07-29 15:13:09 -0400

    fix display

--- logged_in_app.py
+++ logged_in_app.py
@@ -64,129 +64,128 @@ else:
         login_url = "http://ec2-18-191-83-191.us-east-2.compute.amazonaws.com:8080/login"
         logout_url = "http://ec2-18-191-83-191.us-east-2.compute.amazonaws.com:8080/logout"
 
-    if 'button_login_pressed' not in st.session_state:
-        st.session_state.button_login_pressed = False
+        if 'button_login_pressed' not in st.session_state:
+            st.session_state.button_login_pressed = False
 
-    def handle_button_logout_press():
+        if st.button("search"):
+            if job_title or location or company:
+                if frequencies == 'One-Time Instant Results':
+                    job_search_data = {
+                    'title': job_title,
+                    'company': company,
+                    'location': location
+                    }
+                    try:
+                        # Build the URL with query parameters
+                        response = requests.get(
+                            'http://ec2-18-191-83-191.us-east-2.compute.amazonaws.com:8000/instant_search/',
+                            params={
+                                'role': job_title,  # Assuming job_title is used as role
+                                'location': location,
+                                'company': company
+                            }
+                        )
+
+                        if response.status_code == 200:
+                            # Parse the JSON response
+                            results = response.json().get('results', [])
+                            if not results:
+                                st.write("No results found.")
+                            
+                            # Display the results in a formatted way
+                            st.success('Search request sent! Check your results below:')
+                            
+                            for job in results:
+                                title = job.get('title', 'N/A')
+                                company = job.get('company', 'N/A')
+                                location = job.get('location', 'N/A')
+                                link = job.get('link', '#')
+                                if title == 'No matching jobs found.':
+                                    st.write("Sorry, no matching jobs found. Please try another search.")
+                                else:
+                                    st.write(f"**Title:** {title}")
+                                    st.write(f"**Company:** {company}")
+                                    st.write(f"**Location:** {location}")
+                                    st.markdown(f"[Link to Apply]({link})")  # Clickable link
+                                    st.write("---")  # Separator between jobs
+
+                        else:
+                            st.error(f"Failed to add job search. Error: {response.text}")
+                    except Exception as e:
+                        st.error(f"An error occurred: {e}")    
+                elif frequencies:
+                    user_id = st.session_state.user_info['sub']
+                    interval = str(convert_frequency_to_interval(frequencies))
+                    job_search_data = {
+                        'task_id': next_task_id(),
+                        'interval': interval,
+                        'retries': 3,
+                        'created': int(get_current_time()),
+                        'type': "notif",
+                        'user_id': user_id,
+                        'job_id': None,
+                        'title': job_title,
+                        'description': None,
+                        'company': company,
+                        'location': location
+                    }
+
+                    job_search_data['job_id'] = job_search_data['job_id'] if job_search_data['job_id'] is not None else 0
+                    job_search_data['description'] = job_search_data['description'] if job_search_data['description'] is not None else ""
+
+                    try:
+                        fastapi_response = requests.post(
+                            'http://ec2-18-191-83-191.us-east-2.compute.amazonaws.com:8000/add_search/',
+                            json=job_search_data
+                        )
+
+                        if fastapi_response.status_code == 200:
+                            st.success('Search request sent! Check your results shortly.')
+                        else:
+                            st.error(f"Failed to add job search. Error: {fastapi_response.text}")
+
+                    except Exception as e:
+                        st.error(f"An error occurred: {e}")
+
+                    response = sqs.send_message(
+                        QueueUrl=queue_url,
+                        MessageBody=(
+                            json.dumps({
+                                'job_title': job_title,
+                                'location': location,
+                                'company': company
+                            })
+                        )
+                    )
+                else:
+                    st.error("Please choose a notification setting.")
+            else:
+                st.error("Please fill out a field before searching.")
+
+def handle_button_logout_press():
         st.session_state.button_login_pressed = False
         st.markdown(f'<meta http-equiv="refresh" content="0; url={logout_url}">', unsafe_allow_html=True)
 
-    def convert_frequency_to_interval(frequency) -> str:
-        if frequency == 'Every Minute (For Testing)':
-            return "PT1M"
-        elif frequency == 'Daily':
-            return "P1D"
-        elif frequency == 'Weekly':
-            return "P7D"
-        elif frequency == 'Bimonthly':
-            return "P14D"
-        elif frequency == 'Monthly':
-            return "P30D"
-        elif frequency == 'Biweekly':
-            return "P3.5D"
-        else:
-            return "P7D"
+def convert_frequency_to_interval(frequency) -> str:
+    if frequency == 'Every Minute (For Testing)':
+        return "PT1M"
+    elif frequency == 'Daily':
+        return "P1D"
+    elif frequency == 'Weekly':
+        return "P7D"
+    elif frequency == 'Bimonthly':
+        return "P14D"
+    elif frequency == 'Monthly':
+        return "P30D"
+    elif frequency == 'Biweekly':
+        return "P3.5D"
+    else:
+        return "P7D"
 
     # Sidebar for floating menu
     with st.sidebar:
         if st.button("Logout"):
             handle_button_logout_press()
-
-if st.button("search"):
-    if job_title or location or company:
-        if frequencies == 'One-Time Instant Results':
-            job_search_data = {
-            'title': job_title,
-            'company': company,
-            'location': location
-            }
-            try:
-                # Build the URL with query parameters
-                response = requests.get(
-                    'http://ec2-18-191-83-191.us-east-2.compute.amazonaws.com:8000/instant_search/',
-                    params={
-                        'role': job_title,  # Assuming job_title is used as role
-                        'location': location,
-                        'company': company
-                    }
-                )
-
-                if response.status_code == 200:
-                    # Parse the JSON response
-                    results = response.json().get('results', [])
-                    if not results:
-                        st.write("No results found.")
-                    
-                    # Display the results in a formatted way
-                    st.success('Search request sent! Check your results below:')
-                    
-                    for job in results:
-                        title = job.get('title', 'N/A')
-                        company = job.get('company', 'N/A')
-                        location = job.get('location', 'N/A')
-                        link = job.get('link', '#')
-                        if title == 'No matching jobs found.':
-                            st.write("Sorry, no matching jobs found. Please try another search.")
-                        else:
-                            st.write(f"**Title:** {title}")
-                            st.write(f"**Company:** {company}")
-                            st.write(f"**Location:** {location}")
-                            st.markdown(f"[Link to Apply]({link})")  # Clickable link
-                            st.write("---")  # Separator between jobs
-
-                else:
-                    st.error(f"Failed to add job search. Error: {response.text}")
-            except Exception as e:
-                st.error(f"An error occurred: {e}")    
-        elif frequencies:
-            user_id = st.session_state.user_info['sub']
-            interval = str(convert_frequency_to_interval(frequencies))
-            job_search_data = {
-                'task_id': next_task_id(),
-                'interval': interval,
-                'retries': 3,
-                'created': int(get_current_time()),
-                'type': "notif",
-                'user_id': user_id,
-                'job_id': None,
-                'title': job_title,
-                'description': None,
-                'company': company,
-                'location': location
-            }
-
-            job_search_data['job_id'] = job_search_data['job_id'] if job_search_data['job_id'] is not None else 0
-            job_search_data['description'] = job_search_data['description'] if job_search_data['description'] is not None else ""
-
-            try:
-                fastapi_response = requests.post(
-                    'http://ec2-18-191-83-191.us-east-2.compute.amazonaws.com:8000/add_search/',
-                    json=job_search_data
-                )
-
-                if fastapi_response.status_code == 200:
-                    st.success('Search request sent! Check your results shortly.')
-                else:
-                    st.error(f"Failed to add job search. Error: {fastapi_response.text}")
-
-            except Exception as e:
-                st.error(f"An error occurred: {e}")
-
-            response = sqs.send_message(
-                QueueUrl=queue_url,
-                MessageBody=(
-                    json.dumps({
-                        'job_title': job_title,
-                        'location': location,
-                        'company': company
-                    })
-                )
-            )
-        else:
-            st.error("Please choose a notification setting.")
-    else:
-        st.error("Please fill out a field before searching.")
-
 st.title("My Searches")
 
 # AWS DynamoDB configuration
